5f81e275f8c83e76ce5221f27cf82f0e904aa1ec
Emre KAŞ <y.emrekas@outlook.com>
2020-08-09 10:43:54 +0300

    text componenti icin props style eklendi

--- components/BodyText.tsx
+++ components/BodyText.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const BodyText = (props: { children?: any }) => {
-  return <Text style={styles.body}>{props.children}</Text>;
+const BodyText = (props: { children?: any; style?: any }) => {
+  return (
+    <Text style={{ ...styles.body, ...props.style }}>{props.children}</Text>
+  );
 };
 
 export default BodyText;
 
 const styles = StyleSheet.create({
   body: {
-    fontFamily: "open-sans-bold",
+    fontFamily: "open-sans",
   },
 });
